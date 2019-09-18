a4f3578d39939390ec8926f4e324952057f6deb0
Jonny Hopkins <jwhopkins@ucdavis.edu>
2019-09-18 11:41:11 -0700

    slowly understanding routers in express

--- bot.js
+++ bot.js
@@ -20,13 +20,24 @@ console.log('Authentication successful. Running bot...\r\n')
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
-var router = express.Router()
+var router = express.Router();
+
 
 // a middleware function with no mount path. This code is executed for every request to the router
 router.use(function (req, res, next) {
   console.log('Time:', Date.now())
   next()
 });
+
+// a middleware sub-stack shows request info for any type of HTTP request to the /user/:id path
+router.use('/', function (req, res, next) {
+  console.log('Request URL:', req.originalUrl)
+  next()
+}, function (req, res, next) {
+  console.log('Request Type:', req.method)
+  next()
+});
+
 // mount the router on the app
 app.use('/', router);
 app.post('/', (req, res) => {
