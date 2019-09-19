6690c9e5c11f0e08624242f28f839fb769356c6d
Jonny Hopkins <jwhopkins@ucdavis.edu>
2019-09-18 20:37:22 -0700

    added random liking of tweets so it stays awake on heroku

--- bot.js
+++ bot.js
@@ -164,8 +164,8 @@ var pet_meows = [' gives Belle some chin scratches.  \n\
       ——————/———',' gives Belle some chin scratches.  \n\
               ∧__∧　           \n\
 　　     (  ^ . ^ ) (              They feel \n\
-       ____ |  ¥  |__)_________  warm and fuzzy \n\
-             (_\\__/_)                   inside.          \n\
+       ____ |  ¥  |__)_________  warm and  \n\
+             (_\\__/_)             fuzzy inside.          \n\
       ——————/———',' gives Belle some chin scratches.  \n\
               ∧__∧　           \n\
 　　     (  ^ . ^ ) (              She purrs \n\
@@ -194,8 +194,8 @@ var pet_meows = [' gives Belle some chin scratches.  \n\
       ——————/———',' scratches Belle behind her ears.  \n\
               ∧__∧　           \n\
 　　     (  ^ . ^ ) (              They feel \n\
-       ____ |  ¥  |__)_________  warm and fuzzy \n\
-             (_\\__/_)                   inside.          \n\
+       ____ |  ¥  |__)_________  warm and  \n\
+             (_\\__/_)             fuzzy inside.          \n\
       ——————/———',' scratches Belle behind her ears.  \n\
               ∧__∧　           \n\
 　　     (  ^ . ^ ) (              She purrs \n\
@@ -224,8 +224,8 @@ var pet_meows = [' gives Belle some chin scratches.  \n\
       ——————/———',' pats Belle on her head.  \n\
               ∧__∧　           \n\
 　　     (  ^ . ^ ) (              They feel \n\
-       ____ |  ¥  |__)_________  warm and fuzzy \n\
-             (_\\__/_)                   inside.          \n\
+       ____ |  ¥  |__)_________  warm and  \n\
+             (_\\__/_)             fuzzy inside.          \n\
       ——————/———',' pats Belle on her head.  \n\
               ∧__∧　           \n\
 　　     (  ^ . ^ ) (              She purrs \n\
@@ -254,8 +254,8 @@ var pet_meows = [' gives Belle some chin scratches.  \n\
       ——————/———',' pets Belle along her back.  \n\
               ∧__∧　           \n\
 　　     (  ^ . ^ ) (              They feel \n\
-       ____ |  ¥  |__)_________  warm and fuzzy \n\
-             (_\\__/_)                   inside.          \n\
+       ____ |  ¥  |__)_________  warm and  \n\
+             (_\\__/_)              fuzzy inside.          \n\
       ——————/———',' pets Belle along her back.  \n\
               ∧__∧　           \n\
 　　     (  ^ . ^ ) (              She purrs \n\
@@ -284,8 +284,8 @@ var pet_meows = [' gives Belle some chin scratches.  \n\
       ——————/———',' gives Belle belly rubs.  \n\
               ∧__∧　           \n\
 　　     (  ^ . ^ ) (              They feel \n\
-       ____ |  ¥  |__)_________  warm and fuzzy \n\
-             (_\\__/_)                   inside.          \n\
+       ____ |  ¥  |__)_________  warm and  \n\
+             (_\\__/_)             fuzzy inside.          \n\
       ——————/———',' gives Belle belly rubs.  \n\
               ∧__∧　           \n\
 　　     (  ^ . ^ ) (              She purrs \n\
@@ -415,14 +415,32 @@ function add_ending(statusmsg)
 	}
 	return statusmsg;
 }
-
+//every 20 mins 
+int post_count = 0;
 setInterval(function(){ 
-
-
-T.post('statuses/update', { status: add_ending(idle_meows[Math.floor(Math.random()*idle_meows.length)])}, function(err, data, response) {
+if(post_count % 12  == 0)
+{
+	T.post('statuses/update', { status: add_ending(idle_meows[Math.floor(Math.random()*idle_meows.length)])}, function(err, data, response) {
 				console.log("posted tweet!")
 				});
-}, 1000*60*20);
+				
+	int rnd = Math.floor(Math.random() * 100);
+	int rnd_ind = 0;
+	var stream = T.stream('statuses/filter', { track: 'cat' })
+	
+	stream.on('tweet', function (tweet) {
+	if(rnd_ind == rnd)
+	{
+		T.post('favorites/create', { id: tweet.id_str });
+	}
+	rnd_ind++;
+	});
+}
+
+post_count ++;
+
+
+}, 1000*60*2);
 
 var stream = T.stream('statuses/filter', { follow: ['1173977167891456005'] });
 
@@ -431,98 +449,4 @@ function find_usr(currentValue, index, array)
 	
 	return currentValue.id == this;
 }
-
-function tweet_type(m)
-{
-	console.log(m);
-	m = String(m);
-
-	if(m.includes("!pet"))
-		return 0;
-	else if(m.includes("!play"))
-		return 1;
-	else if(m.includes("!feed"))
-		return 2;
-	else
-		return -1;
-}
-stream.on('favorite', function (event) {
-  console.log('boo!');
-})
-
-/*
-stream.on('tweet', function (tweet) {
-			console.log(tweet);
-
-			//valid command entered
-			var type = tweet_type(tweet.text);
-			console.log("type is:" + type);
-			if(type > -1)
-			{
-				console.log("new user!");
-				//new user 
-				if(typeof usr_directory.find(user => user.id === tweet.user.id_str) === 'undefined')
-				{
-					console.log("pushing user!");
-					usr_directory.push({name: tweet.user.screen_name, id: tweet.user.id_str, pet_score: 0, play_score: 0, feed_score: 0, num_visits: 1, visiting: false, visiting_timer: setTimeout(function(){ this.visiting = false; }, 1000*60*1)});
-					
-					
-					
-
-				}
-				
-			}
-			
-			var tg = usr_directory[usr_directory.findIndex(find_usr, tweet.user.id_str)];
-			
-			if(!tg.visiting)
-			{
-				T.post('statuses/update', { status: 'someone is visiting!: @' + tweet.user.screen_name ,
-					}, function(err, data, response) {
-				console.log("posted reply!")
-				});
-				
-			}
-			tg.visiting_timer = setTimeout(function(){ assess_daily_check() }, 1000*60*60*24);
-			
-			switch(type)
-			{
-				//pet the cat
-				case 0:
-				T.post('statuses/update', { status: 'meeoww: @' + tweet.user.screen_name ,
-					}, function(err, data, response) {
-				console.log("posted reply!")
-				});
-				break;
-				
-				//play with the cat
-				case 1:
-				T.post('statuses/update', { status: 'Belle chases a mouse toy: @' + tweet.user.screen_name ,
-					}, function(err, data, response) {
-				console.log("posted reply!")
-				});
-				break;
-				
-				//feed the cat 
-				case 2:
-				T.post('statuses/update', { status: 'You give a treat to Belle. She thanks you!: @' + tweet.user.screen_name ,
-					}, function(err, data, response) {
-				console.log("posted reply!")
-				});
-				break;
-				
-				default:
-				break;
-				
-			}
-			
-				
-				
-			
-			
-})
-		
-stream.on('error', function(err) {
-	throw err;
-});*/
 
