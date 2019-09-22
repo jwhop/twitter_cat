c13fa63583170784ff7d683cc134686a8b2251db
Jonny Hopkins <jwhopkins@ucdavis.edu>
2019-09-21 23:23:14 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -531,7 +531,7 @@ setInterval(function(){
 console.log(post_count);
 if(post_count != 6)
 {
-console.log(“the bot is still active!!”);
+console.log(‘the bot is still active!!’);
 post_count += 1; 
 var stream = T.stream('statuses/filter', { track: ['cat'] });
 }
